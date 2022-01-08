0c71d4bb83b1cfc1b3438f6e55b43a439862dc35
Ömer Faruk İngeç <omer.ingec24@gmail.com>
2022-01-08 23:34:07 +0300

    Initial
    Created Database.
    Compleated group methods.

---
+++ SignalR/SignalR.API/Controllers/NotificationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.API.Hubs;
+using System.Threading.Tasks;
+
+namespace SignalR.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<MyHub> _hunContext;
+
+        public NotificationController(IHubContext<MyHub> hunContext)
+        {
+            _hunContext = hunContext;
+        }
+
+        [HttpGet("{teamCount}")]
+        public async Task<IActionResult> SetTeamCount(int teamCount)
+        {
+            MyHub.TeamCount = teamCount;
+
+            await _hunContext.Clients.All.SendAsync("Notify", $"Arkadaşlar takım {teamCount} kişilik olacaktır.");
+
+            return Ok();
+        }
+    }
+}
---
+++ SignalR/SignalR.API/Models/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.API.Models
+{
+    public class Team
+    {
+        public Team()
+        {
+            Users = new List<User>();
+        }
+
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
